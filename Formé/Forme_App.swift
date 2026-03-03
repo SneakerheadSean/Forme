febd410d90d1dd164ba4991dcdac3b9e45984913
Sean Hughes <sneakerheadsean@gmail.com>
2026-03-02 23:57:37 -0500

    Setting Up

---
+++ Formé/Forme_App.swift
@@ -0,0 +1,17 @@
+//
+//  Forme_App.swift
+//  Formé
+//
+//  Created by Sean Hughes on 3/2/26.
+//
+
+import SwiftUI
+
+@main
+struct Forme_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
